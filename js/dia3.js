1a65a8addb4b170c9a9e53dc5e4561d7dab9e30e
Sergio Marin <higher.vnf@gmail.com>
2012-12-12 20:10:45 -0430

    agregando el range para todos los browsers

--- js/dia3.js
+++ js/dia3.js
@@ -4,8 +4,19 @@ $(document).ready(function() {
 
   // Aqui debe ir nuestro contenido
   $(":range").rangeinput();
+  $(":date").dateinput(); 
+  $("#formualariohtml5").validator();
 
+  /* 
 
+  $(":date").dateinput.localize("es", {
+  months: 'January,February,March,April,May,June,July,August,September,October,November,December',
+  shortMonths:  'Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec',
+  days:         'Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday',
+  shortDays:    'Sun,Mon,Tue,Wed,Thu,Fri,Sat'
+  });
+
+  */
 
 
 });
